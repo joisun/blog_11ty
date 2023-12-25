a7e48d07ab547ded5d762680a9e80e4af8f10b1b
jayce <jaycethanks@outlook.com>
2023-12-25 11:41:02 +0800

    feat: solve image path problem

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,16 @@
 const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
+const path = require("path");
+const markdownItEleventyImg = require("markdown-it-eleventy-img");
 
-module.exports = function(eleventyConfig) {
+
+module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("media");
 
     eleventyConfig.addFilter("formatDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toISODate();
-    });     
+    });
 
     let markdownOptions = {
         html: true,
@@ -18,8 +21,19 @@ module.exports = function(eleventyConfig) {
 
     //Add div around tables
     markdownLib.renderer.rules.table_open = () => '<div class="table-wrapper">\n<table>\n',
-    markdownLib.renderer.rules.table_close = () => '</table>\n</div>',
-
-    eleventyConfig.setLibrary("md", markdownLib);
-
+        markdownLib.renderer.rules.table_close = () => '</table>\n</div>',
+        eleventyConfig.setLibrary("md", markdownLib.use(markdownItEleventyImg, {
+            imgOptions: {
+                widths: [600, 300],
+                urlPath: "/images/",
+                outputDir: "./_site/images/",
+                formats: ["avif", "webp", "jpeg"]
+              },
+              globalAttributes: {
+                class: "markdown-image",
+                decoding: "async",
+                sizes: "100vw"
+              },
+            resolvePath: (filepath, env) => path.join(path.dirname(env.page.inputPath), filepath)
+        }))
 };
---
+++ posts/浏览器如何工作/index.md
@@ -0,0 +1,11 @@
+---
+title: 浏览器如何工作
+date: 2023-12-25
+tags:
+  - post
+---
+## 浏览器如何工作？
+
+![image-20220317145410626](./assets/image-20220317145410626.png)
+
+> [《前端性能优化 原理，工具和方法 - 邓德瑞》](https://www.youtube.com/watch?v=xVD-E4248A8)
