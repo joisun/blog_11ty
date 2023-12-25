db2d1b737b5703072276b320eb23a0934e307175
jayce <jaycethanks@outlook.com>
2023-12-25 17:47:23 +0800

    feat: update syntaxhighlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,18 @@
 const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
 const path = require("path");
+const fs = require("fs");
 const markdownItEleventyImg = require("markdown-it-eleventy-img");
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { full: emoji } = require('markdown-it-emoji')
+
+
 
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("media");
-
     eleventyConfig.addFilter("formatDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toISODate();
     });
@@ -22,18 +27,44 @@ module.exports = function (eleventyConfig) {
     //Add div around tables
     markdownLib.renderer.rules.table_open = () => '<div class="table-wrapper">\n<table>\n',
         markdownLib.renderer.rules.table_close = () => '</table>\n</div>',
-        eleventyConfig.setLibrary("md", markdownLib.use(markdownItEleventyImg, {
-            imgOptions: {
-                widths: [600, 300],
-                urlPath: "/images/",
-                outputDir: "./_site/images/",
-                formats: ["avif", "webp", "jpeg"]
-              },
-              globalAttributes: {
-                class: "markdown-image",
-                decoding: "async",
-                sizes: "100vw"
-              },
-            resolvePath: (filepath, env) => path.join(path.dirname(env.page.inputPath), filepath)
-        }))
+        // markdownItEleventyImg 用于解决图片在post 中链接错误的问题
+        markdownLib.use(emoji);
+    markdownLib.use(require("markdown-it-anchor").default); // Optional, but makes sense as you really want to link to something, see info about recommended plugins below
+    markdownLib.use(require("markdown-it-table-of-contents"));
+    markdownLib.use(markdownItEleventyImg, {
+        imgOptions: {
+            widths: [600, 300],
+            urlPath: "/images/",
+            outputDir: "./_site/images/",
+            formats: ["avif", "webp", "jpeg"]
+        },
+        globalAttributes: {
+            class: "markdown-image",
+            decoding: "async",
+            sizes: "100vw"
+        },
+        resolvePath: (filepath, env) => path.join(path.dirname(env.page.inputPath), filepath)
+    })
+    eleventyConfig.setLibrary("md", markdownLib)
+
+
+    // dev server 404 page
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, bs) {
+                bs.addMiddleware("*", (req, res) => {
+                    const NOT_FOUND_PATH = "_site/404.html";
+                    if (!fs.existsSync(NOT_FOUND_PATH)) {
+                        throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+                    }
+                    const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+                    // Add 404 http status code in request header.
+                    res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
 };
