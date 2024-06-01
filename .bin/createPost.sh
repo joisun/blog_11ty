57467b32dfeebeb6e037d5af2cbd6a34883a2ae2
jaycethanks <jaycethanks@outlook.com>
2024-06-01 23:16:35 +0800

    feat: 更新 newPost 脚本

--- .bin/createPost.sh
+++ .bin/createPost.sh
@@ -1,12 +1,34 @@
 #! /bin/bash
-mkdir ./posts/$(date '+%Y-%m-%d-xxx')
-cd ./posts/$(date '+%Y-%m-%d-xxx')/
+
+# 验证标题是否合法的函数
+validate_title() {
+    local title=$1
+    if [[ "$title" =~ [[:space:][:punct:]] ]]; then
+        echo "标题包含特殊字符或空格，请重新输入。"
+        return 1
+    fi
+}
+
+# 提示用户输入标题，直到输入合法为止
+while true; do
+    read -p "请输入标题: " title
+    if validate_title "$title"; then
+        break
+    fi
+done
+
+# 创建文件夹和切换到该目录
+mkdir "./posts/Others/$(date '+%Y-%m-%d')-$title"
+cd "./posts/Others/$(date '+%Y-%m-%d')-$title/"
+
+# 创建 index.md 文件并创建 assets 文件夹
 touch index.md
 mkdir assets
 
-echo "---
-title: xxx
-date: $(date '+%Y-%m-%d')
-tags:
-  - post
----" > index.md
+# 将标题和其他信息写入 index.md 文件
+echo "---" > index.md
+echo "title: $title" >> index.md
+echo "date: $(date '+%Y-%m-%d')" >> index.md
+echo "tags:" >> index.md
+echo "  - post" >> index.md
+echo "---" >> index.md
