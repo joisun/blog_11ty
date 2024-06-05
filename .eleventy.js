3cb9d19bf344e73f77ea0fcf695793d051cfb203
jaycethanks <jaycethanks@outlook.com>
2024-06-05 11:18:02 +0800

    -

--- .eleventy.js
+++ .eleventy.js
@@ -97,7 +97,8 @@ module.exports = function (eleventyConfig) {
     // pagefind
     // https://rknight.me/blog/using-pagefind-with-eleventy-for-search/
     eleventyConfig.on('eleventy.after', () => {
-        execSync(`npx pagefind --site _site --glob \"**/*.html\"`, { encoding: 'utf-8' })
+        execSync(`pnpm pagefind --site _site --glob \"**/*.html\"`, { encoding: 'utf-8' })
+        // execSync(`npx pagefind --site _site --glob \"**/*.html\"`, { encoding: 'utf-8' })
     })
 
     // page enter before hook
