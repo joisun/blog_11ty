49d0531a30e0a33a33d1c03fcc75208379bf9df9
jaycethanks <jaycethanks@outlook.com>
2024-12-09 17:22:21 +0800

    new post

---
+++ posts/Others/2024-12-09-Tailwindcss_implement_undeiline_effect_with_gradient_background/index.md
@@ -0,0 +1,24 @@
+---
+title: Tailwindcss implement undeiline effect with gradient background
+date: 2024-12-09
+tags:
+  - post
+---
+
+
+
+## Tailwind 通过bg-gradient 实现 underline 下划线效果
+
+```html
+<div class="flex h-screen items-center bg-black p-12 text-3xl font-semibold text-white">
+  <!-- 1.should be inline element -->
+  <!-- 2.parent should not be flex/grid -->
+  <div>
+    <p class="inline bg-gradient-to-b from-transparent from-70% via-orange-500 via-[percentage:70%_70%] to-orange-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore error animi adipisci illo atque dolorum ipsum eius tenetur facilis explicabo, ab suscipit accusantium praesentium aut exercitationem ducimus aperiam unde nam.</p>
+    <div class="my-16"></div>
+    <p class="inline bg-gradient-to-b from-transparent from-90% via-pink-500 via-[percentage:90%_90%] to-pink-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore error animi adipisci illo atque dolorum ipsum eius tenetur facilis explicabo, ab suscipit accusantium praesentium aut exercitationem ducimus aperiam unde nam.</p>
+  </div>
+</div>
+```
+
+![image-20241209172055418](./index.assets/image-20241209172055418.png)
--- posts/React/2024-11-26-实现一个丐版React/examples/SimpleReact/core.js
+++ posts/React/2024-11-26-实现一个丐版React/examples/SimpleReact/core.js
@@ -40,3 +40,83 @@ const SimpleReact = {
 };
 
 export default SimpleReact;
+
+function createDom(fiber) {
+  const dom =
+    fiber.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(fiber.type);
+
+  Object.keys(fiber.props)
+    .filter((prop) => prop !== "children") // children 不是dom 属性，而是子元素
+    .forEach((prop) => {
+      dom[prop] = fiber.props[prop];
+    });
+
+  return dom;
+}
+
+function render(fiber, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+let nextUnitOfWork = null;
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parant.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
