3f6271d44716c36dd4d470ab70bef56c5b87fa3b
sunzhongyi <sunzhongyi@simboss.com>
2025-08-31 15:45:35 +0800

    chore: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,29 @@
 import antfu from '@antfu/eslint-config'
+import html from '@html-eslint/eslint-plugin'
+import htmlParser from '@html-eslint/parser'
 
 export default antfu(
   {
-    formatters: true,
+    stylistic: true,
+    formatters: {
+      html: true,
+    },
+    ignores: ['src/posts/**'],
   },
   {
-    ignores: ['src/posts/**'], // 只排除 posts 目录
+    files: ['**/*.njk'],
+    plugins: {
+      html,
+    },
+    languageOptions: {
+      parser: htmlParser,
+    },
+    rules: {
+      ...html.configs.recommended.rules,
+      'style/max-statements-per-line': 'off',
+      'style/spaced-comment': 'off',
+      'html/require-img-alt': 'off',
+      'html/no-duplicate-in-head': 'off',
+    },
   },
 )
